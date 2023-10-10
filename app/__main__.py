7dda3502c44b2a04a4c30824741b074ca7eebabf
Oswaldo Hernandez <oswaldohernandez@connectiasoluciones.com>
2023-10-10 10:01:31 -0600

    feat: middleware

--- app/__main__.py
+++ app/__main__.py
@@ -5,6 +5,7 @@ import logging
 import logging.config
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .core import config, database, models
 from .routers import auth, posts, users, votes
@@ -12,9 +13,18 @@ from .routers import auth, posts, users, votes
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-models.Base.metadata.create_all(bind=database.engine)
+origins = ["*"]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(posts.router)
